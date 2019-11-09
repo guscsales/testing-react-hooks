69186320e6c8ca1a9232cfac672590ee353a0ff8
Gustavo Sales <gustavosalessantos@gmail.com>
2019-11-09 15:14:55 -0300

    update to git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const { data } = await axios(
+                'https://api.github.com/users/salesgu/repos'
+            );
+
+            setRepos(data);
+        };
+
+        getData();
+
+        return () => alert('Component Will Unmount!!');
+    }, []);
+
+    useEffect(() => {
+        const favoriteRepos = repos.filter(repo => repo.favorite);
+
+        document.title = `You have ${favoriteRepos.length} favorite repos! :)`;
+    }, [repos]);
+
+    const handleFavorite = id => {
+        const newRepos = repos.map(repo =>
+            repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+        );
+
+        setRepos(newRepos);
+    };
+
+    return (
+        <>
+            <ul>
+                {repos.map(({ id, name, favorite }) => (
+                    <li key={id}>
+                        <button onClick={() => handleFavorite(id)}>
+                            Toggle Favorite
+                        </button>{' '}
+                        {id} - {name} -{' '}
+                        <strong>{favorite && 'Is Favorite Repo!'}</strong>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+};
 
 export default App;
